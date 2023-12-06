4fbe76d27369bde706f287ab95702a7dd865c873
DD <dentamarodonato99@gmail.com>
2023-12-06 14:54:10 +0100

    Automation Framework

---
+++ src/main/java/Selenium/Main.java
@@ -0,0 +1,14 @@
+package Selenium;
+
+import drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import utils.FrameworkProperties;
+
+public class Main {
+    public static void main(String[] args) {
+        FrameworkProperties frameworkProperties = new FrameworkProperties();
+        DriverSingleton.getInstance(frameworkProperties.getProperty("browser"));
+        WebDriver driver = DriverSingleton.getDriver();
+        driver.get("https://www.youtube.com/");
+    }
+}
