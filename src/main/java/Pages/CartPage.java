c2732873279871c081ab90789596be71ce4eff04
DD <dentamarodonato99@gmail.com>
2023-12-10 18:12:47 +0100

    Automation Framework

--- src/main/java/Pages/CartPage.java
+++ src/main/java/Pages/CartPage.java
@@ -1,30 +1,38 @@
 package Pages;
 
 import drivers.DriverSingleton;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Constants;
 
 import java.time.Duration;
 
 public class CartPage {
+
     private WebDriver driver;
 
-    public CartPage() {
+    public CartPage(){
         driver = DriverSingleton.getDriver();
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(css = "#post-206 > content > div > div > div.cart-collaterals > div > div > a")
-    private WebElement proceedToCheckoutButton;
+    @FindBy(id = "remove-sauce-labs-backpack")
+    private WebElement removeProduct;
 
-    public void proceedToCheckout(){
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.elementToBeClickable(proceedToCheckoutButton));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", proceedToCheckoutButton);
+    @FindBy(id = "checkout")
+    private WebElement checkOutButton;
+
+    @FindBy(id = "continue-shopping")
+    private WebElement continueShoppingButton;
+
+    public void clickCheckOutButton() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(Constants.TIMEOUT));
+        wait.until(ExpectedConditions.elementToBeClickable(checkOutButton));
+
+        checkOutButton.click();
     }
 }
